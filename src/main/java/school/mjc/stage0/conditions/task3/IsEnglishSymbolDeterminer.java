24cf45eb53d57db139daa0ac3ebad99d4aa4fed2
Xushnud Eshtemirov <xushnud122997@gmail.com>
2024-11-19 12:03:28 +0500

    Finished all tasks

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        boolean isEnglish = Character.isLetter(symbol);
+        if (isEnglish) {
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+        }
     }
 }
